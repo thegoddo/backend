f135be29804635008a1877c4c210e6cc6ac7f8bc
theGoddo <78879232+thegoddo@users.noreply.github.com>
2026-02-20 00:02:25 +0530

    setup nodemail

---
+++ services/EmailService.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+import nodemailer from "nodemailer";
+
+console.log("Email User:", process.env.GMAIL_USER);
+console.log("Email Pass Exists?:", !!process.env.GMAIL_PASSWORD);
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASSWORD,
+  },
+});
+
+export const sendOTP = async (userEmail, otpCode) => {
+  const mailOptions = {
+    from: process.env.GMAIL_USER,
+    to: userEmail,
+    subject: "Your BaatCheet Login OTP",
+    html: `<h3>Your Verification Code is: <b>${otpCode}</b></h3><p>This code expires in 5 minutes.</p>`,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
