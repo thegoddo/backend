bf7836f7e7323a5a07181309bfad7bcc2368e536
theGoddo <78879232+thegoddo@users.noreply.github.com>
2026-02-20 00:02:17 +0530

    otp generator using random method

---
+++ utils/otpGenerator.js
@@ -0,0 +1,5 @@
+export default function generateOTP() {
+  // Generate a random number between 100000 and 999999
+  const otp = Math.floor(100000 + Math.random() * 900000);
+  return otp.toString();
+}
